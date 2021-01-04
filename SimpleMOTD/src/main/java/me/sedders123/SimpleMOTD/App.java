a01bb09ee6f9b4d29e4ee60053711142b50bafde
James Seden Smith <sedders123@gmail.com>
2021-01-04 19:03:54 +0000

    Add reload commands

--- SimpleMOTD/src/main/java/me/sedders123/SimpleMOTD/App.java
+++ SimpleMOTD/src/main/java/me/sedders123/SimpleMOTD/App.java
@@ -6,7 +6,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 
 public final class App extends JavaPlugin implements Listener {
     FileConfiguration config = getConfig();
@@ -19,6 +22,31 @@ public final class App extends JavaPlugin implements Listener {
 
        // Enable our class to check for new players using onPlayerJoin()
        getServer().getPluginManager().registerEvents(this, this);
+
+       getCommand("smotd").setExecutor(this);
+       getCommand("smreload").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        // Handle /smotd reload
+        if (args.length > 0){
+            if(args[0] != "reload"){
+                return false;
+            }
+        }
+
+        if(!(sender instanceof Player) || ((Player)sender).hasPermission("simplemotd.reload")){
+            // Not player so must be console
+            reloadConfig();
+            config = getConfig();
+            Bukkit.getLogger().info("[SimpleMOTD] Config reloaded");
+            if (sender instanceof Player) {
+                sender.sendMessage(ChatColor.GREEN + "[SimpleMOTD] Config reloaded!");
+            }
+            return true;
+        }
+        return false;
     }
 
     // This method checks for incoming players and sends them a message
